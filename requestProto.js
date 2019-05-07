f47a4ed5a6704a7adc0cb8c873264f9c60d45135
Kate <khummle@gmail.com>
2019-05-07 01:04:16 +0000

    Starting the Packages exercise

---
+++ requestProto.js
@@ -0,0 +1,17 @@
+// make a 'GET' request to 'https://sytantris.github.io/http-examples/future.jpg' using chaining. The chain should:
+// Use .on('error') to handle any errors
+// Use .on('response') to log the HTTPS response code, response message (response.statusMessage) and content type (response.headers['content-type'])
+// Use piping and fs.createWriteStream to save the file to your working directory ('./future.jpg')
+
+
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                             // Note 2
+         throw err; 
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode + ", Response Status Message: ", response.statusMessage + response.headers["content-type"]);
+       })
+       .pipe(fs.createWriteStream('./downloaded.html'));               // Note 4
